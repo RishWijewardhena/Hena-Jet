31f59707a7b0eaec757671c3eefa26245d7661b8
RishWijewardhena <rish2001wijewardhana@gmail.com>
2026-08-13 11:29:45 +0530

    test(mesh-reconstruction): refine alpha-shape example
    - Increase surface sampling and use a finer alpha radius for the camera-independent Bunny mesh experiment. [scripts/test/test1.py]

--- scripts/test/test1.py
+++ scripts/test/test1.py
@@ -4,10 +4,10 @@ bunny = o3d.data.BunnyMesh() #
 mesh= o3d.io.read_triangle_mesh(bunny.path)
 mesh.compute_vertex_normals()
 
-pcd= mesh.sample_points_poisson_disk(number_of_points=2000)
+pcd= mesh.sample_points_poisson_disk(number_of_points=20000)
 o3d.visualization.draw_geometries([pcd], point_show_normal=True)
 
-alpha = 0.03
+alpha = 0.005
 print("Alpha shape with alpha =", alpha)
 mesh_alpha = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(pcd, alpha)
 mesh_alpha.compute_vertex_normals()
